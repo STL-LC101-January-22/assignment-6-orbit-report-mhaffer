a5e89e301b439538943ada5063236ea353526338
mhaffer <maddison.haffer@gmail.com>
2022-03-31 11:56:54 -0500

    changes

--- src/app/orbit-counts/orbit-counts.component.spec.ts
+++ src/app/orbit-counts/orbit-counts.component.spec.ts
@@ -5,10 +5,10 @@ import { Satellite } from '../satellite';
 
 import { OrbitCountsComponent } from './orbit-counts.component';
 
+
 describe('OrbitCountsComponent', () => {
   let component: OrbitCountsComponent;
   let fixture: ComponentFixture<OrbitCountsComponent>;
-  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,34 +19,11 @@ describe('OrbitCountsComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrbitCountsComponent);
-    component = fixture.debugElement.componentInstance;
-	 element = fixture.debugElement;
-
-	 component.satellites = [];
-
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('conditionally counts how many satellites are displayed', () => {
-	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
-	fixture.detectChanges();
-	let counts = element.query(By.css('.counts-total'));
-	expect(counts.nativeElement.innerHTML).toBe('1');
-
-	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true), new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
-	fixture.detectChanges();
-	counts = element.query(By.css('.counts-total'));
-	expect(counts.nativeElement.innerHTML).toBe('2');
-
-	component.satellites = [];
-	fixture.detectChanges();
-	counts = element.query(By.css('.counts-total'));
-	expect(counts.nativeElement.innerHTML).toBe('0');
-	
-
-	});
 });
